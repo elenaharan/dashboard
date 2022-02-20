88b147aaf9a3c122ff2735bdddc2ee40cb798cc0
Elena <elenaharan@gmail.com>
2022-02-20 18:58:12 +0000

    implemented bbc rss news feed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Weather from './components/weather/Weather';
+import Title from './components/Title/Title';
 
 function App() {
   return (
     <>
+      <Title name='Elena' />
       <Weather />
       <div className='box'>
         <h2 className='box-title'>News</h2>
-        <rssapp-wall id="XrTdWDLudnFgSo84"></rssapp-wall>
+        <rssapp-wall id="cn6ENfh0uVidcXR4"></rssapp-wall>
       </div>
     </>
   );
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,9 @@
+import './Title.css';
+const Title = ({name}) => {
+
+    return (
+        <h1>Good day {name}</h1>
+    )
+}
+
+export default Title;
